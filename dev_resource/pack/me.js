8574271c7edc6c95f750a4593f1a6e9c3929a22f
pengcheng.yang <806321554@qq.com>
2015-11-23 16:49:47 +0800

    the user center page commit

--- dev_resource/pack/me.js
+++ dev_resource/pack/me.js
@@ -1,9 +1,57 @@
 var React = require("react"),
-	Util = require("../pack/util");
+	Util = require("../pack/util"),
+	Component = require("./component"),
+	Menu = Component.Menu;
 var Info = React.createClass({
+	getInitialState : function(){
+		return {
+			vip : "普通会员",
+			name : "＊＊＊",
+			setting : [
+				{
+					name : "common",
+					value : "普通会员"
+				},
+				{
+					name : "jadeite",
+					value : "翡翠会员"
+				},
+				{
+					name : "platinum",
+					value : "铂金会员"
+				},
+				{
+					name : "diamond",
+					value : "钻石会员"
+				}
+			]
+		}
+	},
+	componentDidMount : function(){
+		$.ajax({
+			url : "/api/getauth",
+			success : function(data){
+				if(data.code !== 400){
+					this.setState({
+						vip : data.vip,
+						name : data.name
+					});
+				}
+			}.bind(this)
+		});
+	},
 	render : function(){
 		return (
-			<div className="info"></div>
+			<div className="info common">
+				{this.state.vip + this.state.name + ",你好!"}
+			</div>
+		);
+	}
+});
+var Asset = React.createClass({
+	render : function(){
+		return (
+			<div className="asset"></div>
 		);
 	}
 });
@@ -18,10 +66,10 @@ var Option = React.createClass({
 	},
 	render : function(){
 		return (
-			<div className="option">
+			<div className={"option" + " " + this.props.data.className}>
 				<i></i>
 				<span>
-					{this.props.name}
+					{this.props.data.name}
 				</span>
 			</div>
 		)
@@ -33,34 +81,42 @@ var Entrance = React.createClass({
 			setting : [
 				{
 					name : "个人信息",
+					className : "account",
 					value : ""
 				},
 				{
 					name : "交易记录",
+					className : "record",
 					value : ""
 				},
 				{
 					name : "我的积分",
+					className : "score",
 					value : ""
 				},
 				{
 					name : "我的红包",
+					className : "bonus",
 					value : ""
 				},
 				{
 					name : "我的加息",
+					className : "ticket",
 					value : ""
 				},
 				{
 					name : "我的邀请",
+					className : "invitation",
 					value : ""
 				},
 				{
 					name : "安全中心",
+					className : "safty",
 					value : ""
 				},
 				{
 					name : "消息中心",
+					className : "infoCenter",
 					value : ""
 				}
 			]
@@ -83,19 +139,39 @@ var Entrance = React.createClass({
 });
 var Page = React.createClass({
 	render : function(){
-		console.log(this.props.data);
 		return (
 			<body>
 				<Info />
+				<Asset />
 				<Entrance />
+				<Menu type={1} option={
+					[
+						{
+							href : "/",
+							text : "首页"
+						},
+						{
+							href : "/product",
+							text : "理财产品"
+						},
+						{
+							href : "/me",
+							text : "我的账户"
+						},
+						{
+							href : "/more",
+							text : "更多"
+						}
+					]
+				} currentIndex={2} />
 			</body>
 		);
 	}
 });
 var init = function(){
-	Util.PageData.setData("/api/getauth", function(data){
+	Util.PageData.setData(null, function(){
 		React.render(
-			<Page data={data} />,
+			<Page />,
 			document.body
 		);
 	}).render(init);
