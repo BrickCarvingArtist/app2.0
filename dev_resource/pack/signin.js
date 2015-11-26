e1c0b320c449e4fa7d9f547d39ebf1654c77ba4e
pengcheng.yang <806321554@qq.com>
2015-11-26 16:21:22 +0800

    the sign in page commit

--- dev_resource/pack/signin.js
+++ dev_resource/pack/signin.js
@@ -1,10 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {PageData} from "./util";
+import {Banner} from "../component/banner";
+class Form extends React.Component{
+	render(){
+		return (
+			<form>
+				<input className="longInput mobile" type="text" placeholder="手机号码" />
+				<input className="longInput password" type="password" placeholder="密码" />
+				<a href="/reset">忘记密码?</a>
+				<input className="longBtn" type="submit" value="确认" />
+				<p>
+					<span>还没账号</span>
+					<a href="/signup">立即注册</a>
+				</p>
+			</form>
+		);
+	}
+}
 class Page extends React.Component{
 	render(){
 		return (
-			<body></body>
+			<body>
+				<Banner data={
+					[
+						{
+							href : "/activity/1",
+							imgSrc : "/images/banner/1.png"
+						},
+						{
+							href : "/activity/2",
+							imgSrc : "/images/banner/2.png"
+						},
+						{
+							href : "/activity/3",
+							imgSrc : "/images/banner/3.png"
+						},
+						{
+							href : "/activity/4",
+							imgSrc : "/images/banner/4.png"
+						},
+						{
+							href : "/activity/5",
+							imgSrc : "/images/banner/5.png"
+						}
+					]
+				} />
+				<Form />
+			</body>
 		);
 	}
 }
